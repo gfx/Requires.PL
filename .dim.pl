2f42ed1619bb9e66b605a36ed65c90c6eb4ff7ad
Fuji Goro <fuji.goro@dena.jp>
2011-10-17 21:01:40 -0700

    Requires.PL

---
+++ .dim.pl
@@ -0,0 +1,7 @@
+# This file is managed by /Users/fuji.goro/perl5/perlbrew/perls/perl-5.14.0/bin/dim.
+{
+  dist => 'Foo',
+  distdir => 'Foo',
+  module => 'Foo',
+  template => 'Default'
+}
---
+++ t/001_basic.t
@@ -0,0 +1,10 @@
+#!perl -w
+use strict;
+use Test::More;
+
+use Foo;
+
+# test Foo here
+pass;
+
+done_testing;
